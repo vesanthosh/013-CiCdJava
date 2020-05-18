eed67a471ea63dd29ac5de57c2932e9faacebacd
Santhosh Venkatesan <vesanthosh@gmail.com>
2020-05-18 11:08:26 +0200

    first commit

---
+++ src/main/java/com/altoros/training/lab/controllers/OverKillService.java
@@ -0,0 +1,7 @@
+package com.altoros.training.lab.controllers;
+
+public class OverKillService {
+  public String concatenate2( String name ) {
+    return "Hello " + name;
+  }
+}
---
+++ src/test/java/com/altoros/training/lab/controllers/OverKillServiceTest.java
@@ -0,0 +1,29 @@
+package com.altoros.training.lab.controllers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OverKillServiceTest {
+
+    OverKillService overKillService = new OverKillService();
+
+    @Test
+    public void testkHappyPath(){
+
+        String actual = "Hello Inigo";
+        String result = overKillService.concatenate2("Inigo");
+        assertEquals(actual, result);
+
+    }
+
+    @Test
+    public void testNoValue(){
+
+        String actual = "Hello Buttercup";
+        String result = overKillService.concatenate2("");
+        assertNotEquals (actual, result);
+
+    }
+
+}
